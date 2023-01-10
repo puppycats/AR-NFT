041683e20f8b4f237d4c84267de8a6e5aa1ca33a
Miandic <misha.andreev.novo@gmail.com>
2023-01-10 15:39:59 +0300

    Random classic

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,28 +1,16 @@
-use std::fs::File;
-use std::io::Write;
 use std::path::Path;
 use rand::Rng;
+use std::string::String;
 
 const WIDTH:usize = 768;
 const HEIGHT:usize = 768;
-const CS:usize = 256;
+const CS:usize = 256-1;
 const CE:usize = 512;
 
 fn main() {
-    let img = random_bg();
+    let img = random();
     
-    
-    let mut img2: Vec<u8> = vec![0; WIDTH*HEIGHT*3];
-    
-    for i in 0..HEIGHT {
-        for j in 0..WIDTH {
-            for k in 0..3 {
-                img2[i*WIDTH*3 + j*3 + k] = img[i][j][k];
-            }
-        }
-    }
-
-    image::save_buffer(&Path::new("image.png"), &img2, WIDTH as u32, HEIGHT as u32, image::ColorType::Rgb8);
+    save_image(img, &String::from("image.png"));
 }
 
 fn random_bg() -> Vec<Vec<Vec<u8>>> {
@@ -35,17 +23,30 @@ fn random_bg() -> Vec<Vec<Vec<u8>>> {
             img[i][j] = vec![t1, t2, t3];
         }
     }
-    return img;
+    img
 }
 
+fn save_image(img: Vec<Vec<Vec<u8>>>, name: &str) {
+    let mut img2: Vec<u8> = vec![0; WIDTH*HEIGHT*3];
+    
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            for k in 0..3 {
+                img2[i*WIDTH*3 + j*3 + k] = img[i][j][k];
+            }
+        }
+    }
+    
+    image::save_buffer(&Path::new(name), &img2, WIDTH as u32, HEIGHT as u32, image::ColorType::Rgb8);
+}
 
-/*
-fn random(img: Vec<Vec<Vec<u8>>>) -> Vec<Vec<Vec<u8>>> {
+fn random() -> Vec<Vec<Vec<u8>>> {
+    let mut img = random_bg();
     for i in CS..CE {
         for j in CS..CE {
             img[i][j] = vec![rand::thread_rng().gen_range(0..=255), rand::thread_rng().gen_range(0..=255), rand::thread_rng().gen_range(0..=255)]
         }
     }
+    img
 }
 
-*/
